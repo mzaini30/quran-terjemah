83d5db573517204820450801872261739ae0a747
mzaini30 <muhzaini30@gmail.com>
2019-03-28 03:18:24 +0800

    oke

--- merancang/_site/app/app.js
+++ merancang/_site/app/app.js
@@ -4,13 +4,13 @@ menuju = function(x){
 	}, 800)
 }
 
-$('.form').on('submit', function(event){
+$('form').on('submit', function(event){
 	event.preventDefault()
 	cari = $(this).find('.cari').val()
-	cari = cari.replace(/\./g, '_')
+	cari = cari.replace(/:/g, '_')
 	menuju('.bagian_' + cari)
 })
 
 $('.navbar-nav a').click(function(){
 	$('.navbar-toggle').click()
-})
+}) 
